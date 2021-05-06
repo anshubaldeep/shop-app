6e5d0dcbe51b623374f2842b37f02b7b475ad9dd
Baldeep Singh <anshubaldeep@gmail.com>
2021-05-06 06:01:15 +0530

    Initial Commit

---
+++ Screens/Shop/ProductDetailsScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+const ProductDetailsScreen=()=>{
+        return(
+            <View>
+
+            </View>
+        );
+};
+
+
+const styles=StyleSheet.create({
+
+});
+
+export default ProductDetailsScreen;
